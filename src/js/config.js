dc5ba3eb134186f11d2ca891cdd9ef929a3185e7
Dzmitry Yurkavets <dzmitry_yurkavets@epam.com>
2013-11-26 14:12:24 +0300

    added google-analytics dep

--- src/js/config.js
+++ src/js/config.js
@@ -3,14 +3,20 @@ require.config({
     baseUrl: chrome.extension.getURL('/js'),
 
     paths: {
-        jquery: 'lib/jquery/jquery',
-        underscore: 'lib/underscore/underscore',
-        text: 'lib/requirejs/text'
+        'google-analytics': 'https://ssl.google-analytics.com/ga',
+        text: '../lib/requirejs/text'
     },
 
     shim: {
+        backbone: {
+            exports: 'Backbone',
+            deps: ['jquery', 'underscore']
+        },
         underscore: {
             exports: '_'
+        },
+        'google-analytics': {
+            exports: '_gaq'
         }
     }
 
