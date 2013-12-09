7df436363abb3881e15a6b7f80d32f12f4a0361e
Dzmitry Yurkavets <dzmitry_yurkavets@epam.com>
2013-12-09 19:25:49 +0300

    initialize panel view

--- src/js/app/content.js
+++ src/js/app/content.js
@@ -1,7 +1,8 @@
 define([
     'app/base',
-    'storage/blacklist'
-], function(App, BlackListStorage) {
+    'storage/blacklist',
+    'view/content/panel'
+], function(App, BlackListStorage, PanelView) {
 
     return App.extend({
 
@@ -12,17 +13,20 @@ define([
         initialize: function(options) {
             console.log('ContentApp#initialize');
             App.prototype.initialize.call(this, options);
-
+            this.initializePanel();
             this.onDocumentChange = this.onDocumentChange.bind(this);
             this.parseDocument = _.throttle(this.parseDocument, options.parsingPeriod);
-
             BlackListStorage.get(function(items) {
                 if (!_.contains(items, location.origin)) {
                     this.start();
                 }
             }.bind(this));
+        },
 
-            this.btn = $('<button>exclude host</button>');
+        initializePanel: function() {
+            var view = this.panel = new PanelView();
+            view.on('close', this.stop, this);
+            view.on('exclude', this.excludeUrl, this);
         },
 
         handleRuntimeMessage: function(message) {
@@ -42,6 +46,10 @@ define([
             this.parseDocument();
         },
 
+        getDocumentContent: function() {
+            return document.body.innerText.replace(this.panel.el.innerText, '');
+        },
+
         parseDocument: function() {
             console.log('ContentApp#parseDocument');
         },
@@ -49,19 +57,17 @@ define([
         stop: function() {
             console.log('ContentApp#stop');
             document.removeEventListener('DOMSubtreeModified', this.onDocumentChange);
-            this.btn.remove();
+            this.panel.remove();
         },
 
         start: function() {
             console.log('ContentApp#start');
-            this.parseDocument();
+            this.panel.render();
             document.addEventListener('DOMSubtreeModified', this.onDocumentChange);
-            $('body').prepend(this.btn);
-            this.btn.on('click', this.exclude.bind(this));
+            this.parseDocument();
         },
 
-        exclude: function() {
-            var url = location.origin;
+        excludeUrl: function(url) {
             console.log('ContentApp#exclude');
             BlackListStorage.add(url, function() {
                 chrome.runtime.sendMessage({ action: 'stopContentApplications', data: url });
