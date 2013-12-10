b1f922c4994f1d4d76a596f6a0c3ab3bab5d187a
Dzmitry Yurkavets <dzmitry_yurkavets@epam.com>
2013-12-10 15:19:26 +0300

    temporarily added underscore mixin

--- src/js/config.js
+++ src/js/config.js
@@ -14,3 +14,11 @@ require.config({
     }
 
 });
+
+_.mixin({
+
+    formatNumber: function(value) {
+        return (Number(value) || 0).toString().split(/(?=(?:\d{3})+(?:\.|$))/g).join(',');
+    }
+
+});
