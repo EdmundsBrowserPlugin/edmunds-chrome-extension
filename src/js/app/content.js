7d26a00b67a22c20a67489f11a870aebf7d462b5
Dzmitry Yurkavets <dzmitry_yurkavets@epam.com>
2013-12-10 15:18:26 +0300

    added special offers

--- src/js/app/content.js
+++ src/js/app/content.js
@@ -1,8 +1,49 @@
 define([
     'app/base',
     'storage/blacklist',
-    'view/content/panel'
-], function(App, BlackListStorage, PanelView) {
+    'view/content/panel',
+    'parser/default'
+], function(App, BlackListStorage, PanelView, Parser) {
+
+    function _parseSpecialOffers(response) {
+        var offers = [];
+        _.each(response.vehicleInventoryGroups, function(vehicleInventoryGroup) {
+            offers = _.union(offers, vehicleInventoryGroup.vehicleInventoryItems);
+        });
+        return offers;
+    }
+
+    function _fetchSpecialOffers(make, model, years, zip) {
+        var deferred = new jQuery.Deferred();
+        $.ajax({
+            url: [
+                'http://www.edmunds.com/api/inventory/v1',
+                make,
+                model,
+                'dealers/newused/list/'
+            ].join('/'),
+            data: {
+                zip: zip,
+                years: years,
+                groupItemsSize: 1,
+                pageSize: 3
+            },
+            dataType: 'json',
+            success: function(response) {
+                deferred.resolve({
+                    make: make,
+                    model: model,
+                    years: years,
+                    zip: zip,
+                    offers: _parseSpecialOffers(response)
+                });
+            },
+            error: function() {
+                deferred.reject();
+            }
+        });
+        return deferred.promise();
+    }
 
     return App.extend({
 
@@ -61,6 +102,41 @@ define([
 
         parseDocument: function() {
             console.log('ContentApp#parseDocument');
+            var documentContent = this.getDocumentContent();
+            if (documentContent === this.previousDocumentContent) {
+                return;
+            }
+            chrome.storage.local.get(['makeModelsMap', 'zip'], function(response) {
+                var vehicles = Parser.parse(documentContent, response.makeModelsMap);
+                this.fetchSpecialOffers(vehicles, response.zip);
+            }.bind(this));
+            this.previousDocumentContent = documentContent;
+        },
+
+        fetchSpecialOffers: function(vehicles, zip) {
+            var requests = [];
+            console.log('ContentApp#fetchSpecialOffers');
+            _.each(vehicles, function(models, make) {
+                _.each(models, function(years, model) {
+                    requests.push(_fetchSpecialOffers(make, model, years, zip));
+                });
+            });
+            return jQuery.when.apply({}, requests).done(function() {
+                var args = [].slice.call(arguments),
+                    offers = {},
+                    offersCount = 0;
+                _.each(args, function(data) {
+                    if (data.offers.length === 0) {
+                        return;
+                    }
+                    if (!offers[data.make]) {
+                        offers[data.make] = {};
+                    }
+                    offers[data.make][data.model] = data.offers;
+                    offersCount += data.offers.length;
+                });
+                this.panel.pricePromise.setSpecialOffers(offers, offersCount);
+            }.bind(this));
         },
 
         stop: function() {
--- src/js/view/content/price-promise.js
+++ src/js/view/content/price-promise.js
@@ -1,19 +1,32 @@
 define([
+    'text!template/content/vehicle-item.html',
+    'text!template/content/special-offer.html',
     'analytics/ga'
-], function(analytics) {
+], function(vehicleItemTemplate, specialOfferTemplate, analytics) {
 
     var
         _activeClass = 'edm-ext-active',
         _disabledClass = 'edm-ext-disabled';
 
+    // precompile templates
+    vehicleItemTemplate = _.template(vehicleItemTemplate);
+    specialOfferTemplate = _.template(specialOfferTemplate);
+
     return Backbone.View.extend({
 
         events: {
-            'click .edm-ext-price-promise-btn': 'onClick'
+            'click .edm-ext-price-promise-btn': 'onClick',
+            'click .edm-ext-vehicles-item': 'onVehicleClick'
         },
 
         initialize: function(options) {
             this[options.enabled ? 'enable' : 'disable']();
+            $(document).on('click', function(event) {
+                var $el = this.$el;
+                if (!$el.is(event.target) && $el.has(event.target).length === 0) {
+                    this.hideDropdown();
+                }
+            }.bind(this));
         },
 
         onClick: function() {
@@ -55,6 +68,51 @@ define([
         showDropdown: function() {
             this.$el.addClass(_activeClass);
             return this;
+        },
+
+        setSpecialOffers: function(map, count) {
+            console.log('#setSpecialOffers');
+            this.$('.edm-ext-price-promise-count').text(count);
+            this[count !== 0 ? 'enable' : 'disable']();
+            this.renderVehicles(map);
+            this.specialOffersMap = map;
+        },
+
+        renderVehicles: function(map) {
+            var list = this.$('.edm-ext-vehicles');
+            list.empty();
+            _.each(map, function(models, make) {
+                _.each(models, function(offers, model) {
+                    var listItem = jQuery(vehicleItemTemplate({
+                        make: make,
+                        model: model,
+                        count: offers.length
+                    }));
+                    listItem.data({
+                        make: make,
+                        model: model,
+                        offers: offers
+                    });
+                    list.append(listItem);
+                });
+            });
+            list.find('> :first-child').trigger('click');
+        },
+
+        onVehicleClick: function(event) {
+            var $el = $(event.currentTarget),
+                offers = $el.data('offers');
+            this.renderSpecialOffers(offers);
+            analytics.track('Vehicles List', 'Click', $el.text());
+        },
+
+        renderSpecialOffers: function(offers) {
+            var list = this.$('.edm-ext-special-offers');
+            list.empty();
+            console.log(offers);
+            _.each(offers, function(offer) {
+                list.append(specialOfferTemplate(offer));
+            });
         }
 
     });
